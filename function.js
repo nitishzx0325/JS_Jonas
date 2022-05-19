0c6c304a61d5e9c555f4df986314148f71a1d94e
nitishzx0325 <nitishzx0325@gmail.com>
2022-05-19 08:34:22 +0530

    Function in js

---
+++ function.js
@@ -0,0 +1,14 @@
+// non parameterise
+function fun() {
+  console.log('Non parametrise ');
+}
+fun();
+function fun(pp) {
+  console.log(' parametrise ', pp);
+}
+fun(121);
+console.log('----------------------------');
+fun('aslkd');
+console.log('-------------------------------');
+fun([12233, 12, 12323, 4323]);
+fun({ name: 'Nitish' });
