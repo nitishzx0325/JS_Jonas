eac8ab9ec373fab209c1d74d0b5a8f11a21444eb
nitishzx0325 <nitishzx0325@gmail.com>
2022-05-19 09:02:24 +0530

    function end

--- function.js
+++ function.js
@@ -12,3 +12,26 @@ fun('aslkd');
 console.log('-------------------------------');
 fun([12233, 12, 12323, 4323]);
 fun({ name: 'Nitish' });
+// IIFE
+(function ff() {
+  console.log('I am IIFE');
+  console.log('I will run automaticaly');
+})();
+
+function pass() {
+  console.log('******************************************');
+  console.log('I habe to pass');
+}
+function give(pass) {
+  console.log('Function which has been passed ', pass);
+}
+give(pass);
+function outer() {
+  console.log('!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!');
+  return function inner() {
+    console.log('I am inner');
+  };
+}
+let v = outer();
+console.log(v);
+v();
